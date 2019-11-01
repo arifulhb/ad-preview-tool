051a75041d794b05c104bb1161f2dde64a37b520
Ariful Haque <arifulhb@gmail.com>
2019-11-01 23:08:13 +0800

    Default Publisher added

---
+++ database/migrations/2019_11_01_101705_create_publishers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePublishersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publishers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 250);
+            $table->string('slug', 250)->uniqid();
+            $table->string('brief')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+            // foreign-key constrain
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publishers');
+    }
+}
