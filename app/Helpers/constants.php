0c9b72b3d4e9b067ff8801cf5c6bf98a51f09ef8
Ariful Haque <arifulhb@gmail.com>
2019-11-02 23:06:30 +0800

    Helper file added

--- app/Helpers/constants.php
+++ app/Helpers/constants.php
@@ -7,3 +7,32 @@ const VISIBILITIES = [
 
 const VISIBILITY_PUBLIC = 5;
 const VISIBILITY_NONE = 0;
+
+/**
+ * Return visibility label
+ *
+ * @param int $value
+ * @return string
+ */
+function getVisibility(int $value) : string
+{
+    return VISIBILITIES[$value];
+}
+
+
+/**
+ * Find Advertisement type in advertisementable_type
+ *
+ * @param string $model
+ * @return void
+ */
+function getAdvertisementType(string $model)
+{
+    $text = explode('\\', $model);
+
+    if (isset($text[2])) {
+        return $text[2];
+    }
+
+    throw new \Exception('Model Name not found in getAdvertisementType()');
+}
