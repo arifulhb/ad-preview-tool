4519a7a1e75802488d5ed3a1fc3ca40aa535a920
Ariful Haque <arifulhb@gmail.com>
2019-11-03 01:41:18 +0800

    Removed unwanted functions

--- app/Contracts/AdvertisementContract.php
+++ app/Contracts/AdvertisementContract.php
@@ -6,9 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 interface AdvertisementContract
 {
-    public function index();
-    public function view(int $id);
     public function create(array $data);
     public function update(array $data, int $id);
-    public function delete(int $id);
 }
