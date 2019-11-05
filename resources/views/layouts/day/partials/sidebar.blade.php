334c571668e3a881099e11b99ed82b12ef7f490d
Ariful Haque <arifulhb@gmail.com>
2019-11-05 14:51:31 +0800

    menu links added

--- resources/views/layouts/day/partials/sidebar.blade.php
+++ resources/views/layouts/day/partials/sidebar.blade.php
@@ -3,11 +3,11 @@
         <i class="fa fa-audio-description text-danger"></i> {{ env('APP_NAME') }}
     </div>
     <div class="list-group list-group-flush">
-        <a href="#" class="list-group-item list-group-item-action bg-light">
+        <a href="{{ url('/home') }}" class="list-group-item list-group-item-action bg-light">
             <i class="fa fa-tachometer-alt"></i>
             {{  __('Dashboard') }}
         </a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">
+        <a href="{{ url('/advertise/new') }}" class="list-group-item list-group-item-action bg-light">
             <i class="fa fa-plus"></i>
             {{ __('New Adv.') }}
         </a>
