19333d8ec53e113fa301a78a24d16082e0f3b641
Ariful Haque <arifulhb@gmail.com>
2019-11-05 17:33:47 +0800

    GoogleTextAd compoennt updated

--- resources/js/components/GoogleTextAd.js
+++ resources/js/components/GoogleTextAd.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import api from '../utils/api'
-import _ from 'lodash'
 
 export default class GoogleTextAd extends Component {
   constructor (props) {
     super(props)
     console.log('GTA props ', props)
-    this.state = {
-    }
   }
 
   render () {
@@ -16,17 +11,17 @@ export default class GoogleTextAd extends Component {
       <div className='google-text-ad mb-5'>
         <div className='headline'>
           <a className='headline__text' href='#'>
-            <span className='headline_text_1'>Awesome Headline 1</span>
-            <span className='headline_text_2 pipe'>Awesome Headline 2</span>
-            <span className='headline_text_3 pipe'>Awesome Headline 3</span>
+            <span className='headline_text_1'>{this.props.ad.headline1}</span>
+            <span className='headline_text_2 pipe'>{this.props.ad.headline2}</span>
+            <span className='headline_text_3 pipe'>{this.props.ad.headline3}</span>
           </a>
         </div>
         <div className='display-url'>
-          <i className='fa fa-ad' /> <span className='display-url_text'>www.example.com/ppc-services</span>
+          <i className='fa fa-ad' /> <span className='display-url_text'>{this.props.ad.displayUrl}</span>
         </div>
         <div className='description'>
-          <span className='description__block_1'>Create Some Amazing Ad Copy Tod.</span>
-          <span className='description__block_2 pipe'>Make Your Ad Stand Out!</span>
+          <span className='description__block_1'>{this.props.ad.description1}</span>
+          <span className='description__block_2 pipe'>{this.props.ad.description2}</span>
         </div>
       </div>
     )
