9bcd2140c709b4906b34ccb3dc4d31827cb3b9ae
Ariful Haque <arifulhb@gmail.com>
2019-11-07 00:42:29 +0800

    Name updated

--- resources/views/layouts/day/partials/sidebar.blade.php
+++ resources/views/layouts/day/partials/sidebar.blade.php
@@ -5,11 +5,11 @@
     <div class="list-group list-group-flush">
         <a href="{{ url('/home') }}" class="list-group-item list-group-item-action bg-light">
             <i class="fa fa-tachometer-alt"></i>
-            {{  __('Dashboard') }}
+            {{  __('My Advertisements') }}
         </a>
         <a href="{{ url('/advertise/new') }}" class="list-group-item list-group-item-action bg-light">
             <i class="fa fa-plus"></i>
-            {{ __('New Adv.') }}
+            {{ __('New Advertisement') }}
         </a>
     </div>
 </div>
